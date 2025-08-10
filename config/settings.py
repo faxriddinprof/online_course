1ba7b556aebce2ffca0e17fb795aae349f18780e
Faxriddin Baxtiyorov <faxriddinbaxtiyorov144@gmail.com>
2025-08-11 00:11:39 +0500

    staticfiles

--- config/settings.py
+++ config/settings.py
@@ -133,6 +133,7 @@ STATIC_URL = "static/"
 STATICFILES_DIRS = [ os.path.join(BASE_DIR, 'static') ]
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 
 AUTH_USER_MODEL = 'users.CustomUser'
