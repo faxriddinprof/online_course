a13c9396db4ea17c218bb8fb1c0d9a68124a51a4
Faxriddin Baxtiyorov <faxriddinbaxtiyorov144@gmail.com>
2025-08-13 21:10:58 +0500

    settings was changed

--- config/settings.py
+++ config/settings.py
@@ -138,6 +138,7 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 # Media files
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+# MEDIA_ROOT = os.path.join(BASE_DIR, 'static/img')
 
 AUTH_USER_MODEL = 'users.CustomUser'
 
